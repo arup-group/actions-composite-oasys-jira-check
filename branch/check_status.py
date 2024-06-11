9a7f2b8b847cdcc3cacdc94d8d26457025043263
René Fritze <rene.fritze@arup.com>
2024-06-11 15:43:21 +0200

    OQS-213| adjust to tighter ruff config

--- branch/check_status.py
+++ branch/check_status.py
@@ -1,7 +1,8 @@
+import json
 import os
 import re
+
 import requests
-import json
 
 
 def get_inputs():
@@ -11,7 +12,8 @@ def get_inputs():
     dotenv.load_dotenv()
     branch_to_check = os.getenv("INPUT_BRANCH_TO_CHECK")
     valid_branch_names = os.getenv(
-        "INPUT_VALID_BRANCH_NAMES", "task|test|bugfix|feature|hotfix|epic"
+        "INPUT_VALID_BRANCH_NAMES",
+        "task|test|bugfix|feature|hotfix|epic",
     )
     jira_username = os.getenv("INPUT_JIRA_USERNAME")
     jira_password = os.getenv("INPUT_JIRA_PASSWORD")
@@ -21,10 +23,12 @@ def get_inputs():
 def extract_issue_key(branch_to_check, valid_branch_names):
     # Check the branch format
     match = re.match(
-        rf"^(refs/heads/)?({valid_branch_names})/([a-zA-Z0-9]+-[0-9]+)", branch_to_check
+        rf"^(refs/heads/)?({valid_branch_names})/([a-zA-Z0-9]+-[0-9]+)",
+        branch_to_check,
     )
     if not match:
-        raise RuntimeError("Branch format is incorrect")
+        msg = "Branch format is incorrect"
+        raise RuntimeError(msg)
     return match.group(3)
 
 
@@ -37,6 +41,7 @@ def query_jira_api(issue_key, jira_username, jira_password):
         f"https://ovearup.atlassian.net/rest/api/3/issue/{issue_key}",
         headers=headers,
         auth=(jira_username, jira_password),
+        timeout=10,
     )
     response.raise_for_status()
     return response
@@ -44,13 +49,12 @@ def query_jira_api(issue_key, jira_username, jira_password):
 
 def check_status_category(response):
     # Check the status category
-    issue_status_cat = json.loads(response.text)["fields"]["status"]["statusCategory"][
-        "id"
-    ]
+    issue_status_cat = json.loads(response.text)["fields"]["status"]["statusCategory"]["id"]
     in_progress = 4
     if issue_status_cat != in_progress:
+        msg = f"Status category is not 'In Progress', but {issue_status_cat}"
         raise RuntimeError(
-            f"Status category is not 'In Progress', but {issue_status_cat}"
+            msg,
         )
 
 
